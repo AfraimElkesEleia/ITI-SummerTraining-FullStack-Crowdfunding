cd982400ed9567c17a1b8f8e45c4899efd507c68
Afraim Elkes Eleia <afraimeleia200@gmail.com>
2025-08-30 11:52:55 +0300

    - Done v1 of register screen - Done Login screen - store data of user and uplaod image through postgres

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.register,name='register')
-]
+    path('',views.register_view,name='register'),
+    path('login/',views.login_view,name='login'),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,23 @@
 # views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 from django.contrib import messages
 from .forms import CustomUserCreationForm
 
-def login(request):
-    render(request,'lgoin.html')
-def register(request):
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email") 
+        password = request.POST.get("password")
+        user = authenticate(request,email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect("home") 
+        else:
+            messages.error(request, "Invalid username or password.")
+    return render(request, "pages/login.html")
+
+def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
